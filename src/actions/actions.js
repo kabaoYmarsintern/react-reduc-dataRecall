a1cada9c5048615a301348595b839c485fcf4bf0
kabaoYmarsintern <94191447+kabaoYmarsintern@users.noreply.github.com>
2022-01-02 15:11:47 -0600

    Initial commit

---
+++ src/actions/actions.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+export const fetchPosts = () => async (dispatch, getState) => {
+  dispatch({ type: "FETCH_POSTS_REQUEST" });
+  try {
+    const response = await axios.get("http://worldcup.sfg.io/matches");
+    dispatch({
+      type: "FETCH_POSTS_SUCCESS",
+      items: response.data
+    });
+  } catch (error) {
+    dispatch({
+      type: "FETCH_POSTS_FAILURE",
+      error
+    });
+  }
+};
